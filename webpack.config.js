652b3d7973079412fd30bfdc796fafab7d350e46
shinhyojeong <hyojeonge0@naver.com>
2022-08-11 23:27:22 +0900

    🔧 config: 웹팩 설치 및 설정

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "static" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+  },
+};
