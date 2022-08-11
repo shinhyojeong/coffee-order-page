df10be77c1e761e78785e72aa39d6fc3f5ac1b6a
shinhyojeong <hyojeonge0@naver.com>
2022-08-12 00:07:17 +0900

    🔧 config: TypeScript 설정

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,11 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env', '@babel/preset-typescript']
             }
+          },
+          {
+            loader: 'ts-loader'
           }
         ]
       }
